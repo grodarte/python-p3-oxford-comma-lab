2778513be26ac7b0777bebf591b8426207a842b0
Gabrielle <ms.grodarte@gmail.com>
2025-01-14 22:38:34 -0800

    complete lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,14 @@
 def oxford_comma(items):
-    return None
+    # joins items with a comma
+    # also inserts and before the last item
+    if len(items) < 3:
+        return " and ".join(items)
+    else:
+        last_word = items.pop(-1)
+        ox_sent = ", ".join(items)
+        ox_sent += f", and {last_word}"
+        return ox_sent
+
+
+        
+
